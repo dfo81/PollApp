dd1bba6bc7900e85c19ec21ffda41173483c20a9
Dieter Foos <d.foos@mail.de>
2026-07-06 20:04:04 +0200

    surveys component added

--- src/app/components/home/home.ts
+++ src/app/components/home/home.ts
@@ -1,9 +1,9 @@
 import { Component } from '@angular/core';
-import { Header } from '../header/header';
+import { Surveys } from '../surveys/surveys';
 
 @Component({
   selector: 'app-home',
-  imports: [Header],
+  imports: [Surveys],
   templateUrl: './home.html',
   styleUrl: './home.scss',
 })
