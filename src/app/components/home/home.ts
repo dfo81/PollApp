d9f92c166f80b3b90bdf4222e389ee95682f02b4
Dieter Foos <d.foos@mail.de>
2026-07-26 21:20:21 +0200

    create new servey added

--- src/app/components/home/home.ts
+++ src/app/components/home/home.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
 import { Surveys } from '../surveys/surveys';
 
 @Component({
   selector: 'app-home',
-  imports: [Surveys],
+  imports: [Surveys, RouterLink],
   templateUrl: './home.html',
   styleUrl: './home.scss',
 })
